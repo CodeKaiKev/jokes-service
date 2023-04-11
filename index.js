f9e8512a5a497cce8d04f659f94ce56a3876c2cd
Codekaikev <codekaikev@gmail.com>
2023-04-11 08:42:21 +0100

    New

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
-
+const { Joke,  Op } = require('./db');
+// const {Op} = require("sequelize");
+app.set("json spaces", "\t");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
+
+  if(req.query) {
+    try {
+      // TODO - filter the jokes by tags and content
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]: req.query.tags ?? "",
+          },
+          joke: {
+            [Op.substring]: req.query.content ?? "",
+          }
+          
+        },
+      });
+      res.send(jokes);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+
+    return;
+  }
+
+
+  // if(req.query.content) {
+  //   try {
+  //     // TODO - filter the jokes by tags and content
+  //     const jokes = await Joke.findAll({
+  //       where: {
+  //         joke: {
+  //           [Op.substring]: req.query.content,
+  //         }
+          
+  //       },
+  //     });
+  //     res.send(jokes);
+  //   } catch (error) {
+  //     console.error(error);
+  //     next(error);
+  //   }
+
+  //   return;
+  // }
+
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const jokes = await Joke.findAll();
     res.send(jokes);
   } catch (error) {
     console.error(error);
